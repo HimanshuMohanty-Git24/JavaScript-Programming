286a40d20c81b6df9292ceaac16d2a3ddc70c4f9
Himanshu Mohanty <codehimanshu24@gmail.com>
2024-06-02 19:20:12 +0530

    feat: Create basic API server with routing and logging

---
+++ server/index.js
@@ -0,0 +1,32 @@
+//making a api server
+
+const http = require('http');
+const fs = require('fs');
+const { log } = require('console');
+
+const myServer = http.createServer((req,res) => {
+    const log =`${Date.now()}: ${req.url} New Req Received\n`;
+    fs.appendFile('log.txt',log, (err) => {
+        switch(req.url){
+            case '/':
+                res.writeHead(200,{'Content-Type':'text/html'});
+                res.end('<h1>Home Page</h1>');
+                break;
+            case '/about':
+                res.writeHead(200,{'Content-Type':'text/html'});
+                res.end('<h1>About Page</h1>');
+                break;
+            case '/data':
+                res.writeHead(200,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({message:'Hello World'}));
+                break;
+            default:
+                res.writeHead(404,{'Content-Type':'text/html'});
+                res.end('<h1>Page Not Found</h1>');
+        }
+    });
+});
+
+myServer.listen(8000, () => {
+    console.log('Server is running on port 8000');
+});
