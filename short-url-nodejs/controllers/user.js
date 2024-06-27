4036050792edd99faf85027d6ab80d2121adfdb0
Himanshu Mohanty <codehimanshu24@gmail.com>
2024-06-27 10:47:35 +0530

    refactor: Update user login handling to use JWT token instead of session ID

--- short-url-nodejs/controllers/user.js
+++ short-url-nodejs/controllers/user.js
@@ -21,9 +21,8 @@ async function handleUserLogin(req, res) {
       error: "Invalid Username or Password",
     });
 
-  const sessionId = uuidv4();
-  setUser(sessionId, user);
-  res.cookie("uid", sessionId);
+  const token = setUser(user);
+  res.cookie("uid", token);
   return res.redirect("/");
 }
 
--- short-url-nodejs/service/auth.js
+++ short-url-nodejs/service/auth.js
@@ -1,11 +1,16 @@
-const sessionIdToUserMap = new Map();
+const jwt = require('jsonwebtoken');
+const secret ="Hkm@8426"
 
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+function setUser(user) {
+  return jwt.sign({
+    _id: user._id,
+    email: user.email,
+  }, secret);
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if(!token) return null;
+  return jwt.verify(token, secret);
 }
 
 module.exports = {
